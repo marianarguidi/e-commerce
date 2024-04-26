23a09d628e8b6df9f9ffd7b5bc2b5f96525020ac
ribasmariana <mariana.248.ribas@gmail.com>
2024-04-26 20:39:53 -0300

    teste

---
+++ projeto-ecommerce/src/test/java/br/com/senac/projetoecommerce/ProjetoEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.projetoecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
