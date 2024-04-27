30ef5b1a94c5886b6112f219682877aa1629588a
ribasmariana <mariana.248.ribas@gmail.com>
2024-04-27 13:17:58 -0300

    Estrutura e Clientes Entity

---
+++ projeto-ecommerce/src/main/java/br/com/senac/projetoecommerce/entitys/Clientes.java
@@ -0,0 +1,73 @@
+package br.com.senac.projetoecommerce.entitys;
+
+import jakarta.persistence.*;
+
+@Entity (name = "clientes")
+public class Clientes {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String nome;
+
+    @Column (nullable = false)
+    private String sobrenome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column (nullable = false)
+    private String telefone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+//
+//    public List<Enderecos> getEnderecos() {
+//        return enderecos;
+//    }
+//
+//    public void setEnderecos(List<Enderecos> enderecos) {
+//        this.enderecos = enderecos;
+//    }
+
+}
