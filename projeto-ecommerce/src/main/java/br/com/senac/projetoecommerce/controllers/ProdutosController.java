3dafe32b6f95cd9661fa13688e1938cd6e681bd9
ribasmariana <mariana.248.ribas@gmail.com>
2024-05-14 20:44:42 -0300

    produtos

--- projeto-ecommerce/src/main/java/br/com/senac/projetoecommerce/controllers/ProdutosController.java
+++ projeto-ecommerce/src/main/java/br/com/senac/projetoecommerce/controllers/ProdutosController.java
@@ -4,47 +4,79 @@ import br.com.senac.projetoecommerce.entitys.Carrinho;
 import br.com.senac.projetoecommerce.entitys.Produtos;
 import br.com.senac.projetoecommerce.useCases.Produtos.Categorias;
 import br.com.senac.projetoecommerce.useCases.Produtos.ProdutoService;
-import br.com.senac.projetoecommerce.useCases.Produtos.domains.ProdutosResponseDom;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin
-    @Controller
-    @RequestMapping("/listaProdutos")
-    public class ProdutosController {
-        @Autowired
-        private ProdutoService produtosService;
+@RestController
+@RequestMapping("/listaProdutos") // Mapeie o prefixo do caminho para todos os endpoints neste controlador
+public class ProdutosController {
 
-        //carregar todos os produtos por categoria
-        @GetMapping("/carregar/{categoria}")
-        public ResponseEntity<ProdutosResponseDom> carregarProdutos(@PathVariable Categorias categoria){
-            try {
-                ProdutosResponseDom responseDom = (ProdutosResponseDom) produtosService.carregarProdutosCategoria(categoria);
-                if (responseDom != null){
-                    return ResponseEntity.ok(responseDom);
-                }
-                return ResponseEntity.badRequest().body(null);
-            } catch (IllegalArgumentException e) {
-                return ResponseEntity.badRequest().body(null);
-            } catch (Exception e){
-                e.printStackTrace();
-                return ResponseEntity.badRequest().body(null);
-            }
-        }
+    @Autowired
+    private ProdutoService produtoService;
 
-        //adicionar produto ao carrinho
-        @PostMapping("/adicionarAoCarrinho/{produtoId}")
-        public ResponseEntity<String> adicionarAoCarrinho(@PathVariable Long produtoId, @RequestBody Carrinho carrinho) {
-            try {
-                ProdutoService produtoService = null;
-                Produtos produto = produtoService.obterProdutoPorId(produtoId);
-                produtoService.atualizarProduto(produto, carrinho);
-                return ResponseEntity.ok("Produto adicionado ao carrinho com sucesso.");
-            } catch (Exception e) {
-                e.printStackTrace();
-                return ResponseEntity.badRequest().body("Erro ao adicionar produto ao carrinho.");
+    @GetMapping("/carregar/{categoria}")
+    public ResponseEntity<List<Produtos>> carregarProdutos(@PathVariable String categoria) {
+        try {
+            // Convertendo a string da categoria para enumeração
+            Categorias categoriaEnum = Categorias.valueOf(categoria.toUpperCase());
+
+            // Carregar os produtos por categoria usando o serviço
+            List<Produtos> produtos = produtoService.carregarProdutosPorCategoria(categoriaEnum);
+
+            // Verifica se a lista de produtos não está vazia
+            if (!produtos.isEmpty()) {
+                return ResponseEntity.ok(produtos);
+            } else {
+                // Retorna um erro 404 caso não existam produtos para a categoria especificada
+                return ResponseEntity.notFound().build();
             }
+        } catch (IllegalArgumentException e) {
+            // Retorna um erro 400 caso a categoria fornecida não exista
+            return ResponseEntity.badRequest().build();
+        } catch (Exception e) {
+            // Retorna um erro 500 caso ocorra algum erro interno no servidor
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+    @PostMapping("/adicionarAoCarrinho/{produtoId}")
+    public ResponseEntity<String> adicionarAoCarrinho(@PathVariable Long produtoId, @RequestBody Carrinho carrinho) {
+        try {
+            // Verifica se o carrinho já existe ou cria um novo carrinho
+            if (carrinho == null) {
+                carrinho = new Carrinho();
+                carrinho.setValorTotal(0.0); // Define o valor total inicial como 0
+                carrinho.setQuantidadeProdutos(0); // Define a quantidade de produtos inicial como 0
+            }
+
+            // Obtém o produto pelo ID
+            Produtos produto = produtoService.obterProdutoPorId(produtoId);
+
+            // Adiciona o produto ao carrinho
+            produto.setCarrinho(carrinho);
+            carrinho.getProdutos().add(produto);
+
+            // Atualiza o valor total e a quantidade de produtos do carrinho
+            double valorTotalCarrinho = carrinho.getValorTotal() + produto.getValor();
+            int quantidadeProdutosCarrinho = carrinho.getQuantidadeProdutos() + 1;
+            carrinho.setValorTotal(valorTotalCarrinho);
+            carrinho.setQuantidadeProdutos(quantidadeProdutosCarrinho);
+
+            // Atualiza o produto no serviço
+            produtoService.atualizarProduto(produto);
+
+            return ResponseEntity.ok("Produto adicionado ao carrinho com sucesso.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body("Erro ao adicionar produto ao carrinho.");
+        }
+    }
+
+}
--- projeto-ecommerce/src/main/java/br/com/senac/projetoecommerce/useCases/Produtos/ProdutoService.java
+++ projeto-ecommerce/src/main/java/br/com/senac/projetoecommerce/useCases/Produtos/ProdutoService.java
@@ -1,16 +1,11 @@
 package br.com.senac.projetoecommerce.useCases.Produtos;
 
 import br.com.senac.projetoecommerce.entitys.Carrinho;
-import br.com.senac.projetoecommerce.entitys.Enderecos;
 import br.com.senac.projetoecommerce.entitys.Produtos;
-import br.com.senac.projetoecommerce.useCases.Produtos.domains.ProdutosResponseDom;
 import br.com.senac.projetoecommerce.useCases.Produtos.implement.ProdutoRepository;
-import br.com.senac.projetoecommerce.useCases.enderecos.domains.EnderecosResponseDom;
-import br.com.senac.projetoecommerce.useCases.enderecos.implement.EnderecosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,26 +14,13 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    //carregar os produtos de apenas uma categoria
-    public List<ProdutosResponseDom> carregarProdutosCategoria(Categorias categorias) {
-        List<Produtos> resultado = produtoRepository.findByCategoria(categorias);
-        List<ProdutosResponseDom> produtosResponseDomList = new ArrayList<>();
-
-        for (Produtos dado : resultado) {
-            ProdutosResponseDom produtosResponseDom = new ProdutosResponseDom();
-            produtosResponseDom.setId(dado.getId());
-            produtosResponseDom.setValor(dado.getValor());
-            produtosResponseDom.setDescricao(dado.getDescricao());
-            produtosResponseDom.setDetalhes(dado.getDetalhes());
-            produtosResponseDom.setQuantidade(dado.getQuantidade());
-
-            produtosResponseDomList.add(produtosResponseDom);
-        }
-        return produtosResponseDomList;
+    public List<Produtos> carregarProdutosPorCategoria(Categorias categoria) {
+        return produtoRepository.findByCategoria(categoria);
     }
 
     //atualiza estoque do produto e adiciona ao carrinho
-    public void atualizarProduto(Produtos produto, Carrinho carrinho) {
+    public void atualizarProduto(Produtos produto) {
+        Carrinho carrinho = null;
         if ((carrinho.getQuantidadeProdutos() < 5) && (produto.getQuantidade() > 0)) {
             produto.setQuantidade(produto.getQuantidade() - 1);
             carrinho.setProdutos(carrinho.getProdutos());
