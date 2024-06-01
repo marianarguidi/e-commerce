d086f2d483b2b1c24f21d48e6e800aaaba5180f5
Maria Eduarda Teixeira Silvano <mariaeduardat.silvano@gmail.com>
2024-06-01 18:59:01 -0300

    criação de algumas classes, metodos e atributos (vou revisá-los)

--- projeto-ecommerce/src/main/java/br/com/senac/projetoecommerce/controllers/ProdutoController.java
+++ projeto-ecommerce/src/main/java/br/com/senac/projetoecommerce/controllers/ProdutoController.java
@@ -2,8 +2,10 @@ package br.com.senac.projetoecommerce.controllers;
 
 import br.com.senac.projetoecommerce.entitys.Produtos;
 import br.com.senac.projetoecommerce.useCases.produtos.ProdutoService;
+import br.com.senac.projetoecommerce.useCases.produtos.ProdutosResponseDom;
 import br.com.senac.projetoecommerce.utils.Categorias;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,8 +21,40 @@ public class ProdutoController {
         return produtoService.listarProdutosPorCategoria(categoria);
     }
 
-    @GetMapping("/{id}")
+    //@GetMapping("/{id}")
     public Produtos buscarProdutoPorId(@PathVariable Long id) {
         return produtoService.buscarProdutoPorId(id);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> carregarProdutoById(@PathVariable Long id) {
+        try {
+            ProdutosResponseDom out = produtoService.carregarProdutoById(id);
+
+            if (out != null) {
+                return ResponseEntity.ok(out);
+            }
+
+            return ResponseEntity.noContent().build();
+        } catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("Erro não mapeado: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> carregarProdutos(@RequestParam(required = false) String descricao) {
+        try {
+            List<ProdutosResponseDom> out = produtoService.carregarProdutos(descricao);
+
+            if (!out.isEmpty()) {
+                return ResponseEntity.ok(out);
+            }
+
+            return ResponseEntity.noContent().build();
+        } catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("Erro não mapeado: " + e.getMessage());
+        }
     }
 }
---
+++ projeto-ecommerce/src/main/java/br/com/senac/projetoecommerce/useCases/produtos/ProdutosMappers.java
@@ -0,0 +1,19 @@
+package br.com.senac.projetoecommerce.useCases.produtos;
+import br.com.senac.projetoecommerce.entitys.Produtos;
+
+public class ProdutosMappers {
+    public static ProdutosResponseDom produtosParaProdutosResponseDom(Produtos in){
+        ProdutosResponseDom out = new ProdutosResponseDom();
+        out.setId(in.getId());
+        //out.setCodigoProduto(in.getCodigoProduto());
+        out.setDescricao(in.getDescricao());
+        //out.setNome(in.getNome());
+        //out.setImagemPequena(in.getImagemPequena());
+        //out.setImagemGrande(in.getImagemGrande());
+        //out.setCreatedAt(in.getCreatedAt());
+        out.setPreco(!in.getPrecos().isEmpty() ? in.getPrecos().get(0).getPreco() : 0.0);
+        //out.setPreco(in.getPreco());
+
+        return out;
+    }
+}
